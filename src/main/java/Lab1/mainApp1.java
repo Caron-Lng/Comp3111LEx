bd23f50be4f95c6e195c636c15654619067e421c
Caron <caronng5565@gmail.com>
2023-09-10 21:47:49 +0800

    Add 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -1,5 +1,8 @@
 package Lab1;
 public class mainApp1 {
+    public mainApp1(){
+    }
+
     public static void main(String[] args){
         int b=2;
         int n =11;
@@ -7,6 +10,6 @@ public class mainApp1 {
         System.out.println("Program stated...");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+myLibrary.factorial(n));
-        System.out.println("Program ..Ended ...");
+        System.out.println("Program Ended ...");
     }
 }
